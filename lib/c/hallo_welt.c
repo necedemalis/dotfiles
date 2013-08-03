8c80d248964a485dec60677a84c64a24fd5e3391
necedemalis <Chief_Wiggum44@web.de>
2013-08-03 12:15:21 +0200

    Changed dotfiles

---
+++ lib/c/hallo_welt.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+
+int main() {
+        printf("Hallo Welt\n");
+        printf("Hallo Du\n");
+        return 0;
+}
---
+++ lib/python/making_games_with_python/03/memorypuzzle2.py
@@ -0,0 +1,31 @@
+import random, sys
+from pygame.locals import *
+
+FPS = 30
+WINDOWWIDTH = 640 # size of window's width in pixels
+WINDOWHEIGHT = 480 # size of windows' height in pixels
+REVEALSPEED = 8 # speed boxes' sliding reveals and covers
+BOXSIZE = 40 # size of box height & width in pixels
+GAPSIZE = 10 # size of gap between boxes in pixels
+BOARDWIDTH = 10 # number of columns of icons
+BOARDHEIGHT = 7 # number of rows of icons
+assert (BOARDWIDTH*BOARDHEIGHT) % 2 == 0, 'Board must have an even number of matches.'
+XMARGIN = int((WINDOWWIDTH - (BOARDWIDTH * (BOXSIZE + GAPSIZE))) / 2)
+YMARGIN = int((WINDOWHEIGHT - (BOARDHEIGHT * (BOXSIZE + GAPSIZE))) / 2)
+
+#            R    G    B
+GRAY     = (100, 100, 100)
+NAVYBLUE = ( 60,  60, 100)
+WHITE    = (255, 255, 255)
+RED      = (255,   0,   0)
+GREEN    = (  0, 255,   0)
+BLUE     = (  0,   0, 255)
+YELLOW   = (255, 255,   0)
+ORANGE   = (255, 128,   0)
+PURPLE   = (255,   0, 255)
+CYAN     = (  0, 255, 255)
+
+BGCOLOR = NAVYBLUE
+LIGHTBGCOLOR = GRAY
+BOXCOLOR = WHITE
+HIGHLIGHTCOLOR = BLUE
